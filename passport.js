5a4d46e5325fc7caacd023db457146e7e47cce90
Stuart Hardie <49949807+stuartfreaks@users.noreply.github.com>
2023-01-11 11:24:17 +0100

    Update passport.js

--- passport.js
+++ passport.js
@@ -15,7 +15,7 @@ passport.use(
     },
     (username, password, callback) => {
       console.log(username + " " + password);
-      Users.findOne({ Username: username }, (error, user) => {
+      Users.findOne({ username: username }, (error, user) => {
         if (error) {
           console.log(error);
           return callback(error);
